747226bfaa4aec0f55cf0bf5a623a7eaf31d0daa
ysobj <litailang+ysobj@gmail.com>
2016-10-02 12:34:17 +0900

    added testCombination.

--- src/test/java/me/ysobj/sqlparser/parser/ParserTest.java
+++ src/test/java/me/ysobj/sqlparser/parser/ParserTest.java
@@ -39,4 +39,15 @@ public class ParserTest {
 		assertThat(parser.parse(new Tokenizer("select update delete")), not(nullValue()));
 		assertThat(parser.parse(new Tokenizer("select delete update")), is(nullValue()));
 	}
+
+	@Test
+	public void testCombination() {
+		// (A|B)CD
+		Parser ab = new ChoiseParser(new KeywordParser("A"), new KeywordParser("B"));
+		Parser parser = new SequenceParser(ab, new KeywordParser("C"), new KeywordParser("D"));
+		assertThat(parser.parse(new Tokenizer("A C D")), not(nullValue()));
+		assertThat(parser.parse(new Tokenizer("B C D")), not(nullValue()));
+		assertThat(parser.parse(new Tokenizer("A B C D")), is(nullValue()));
+		assertThat(parser.parse(new Tokenizer("C D")), is(nullValue()));
+	}
 }
